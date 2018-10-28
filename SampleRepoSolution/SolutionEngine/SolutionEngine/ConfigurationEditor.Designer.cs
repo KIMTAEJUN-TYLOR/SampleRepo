f6383e3728b98261ba011d1c481ade96ecddd7ac
KIM TAEJUN <kim810414@naver.com>
2018-10-27 21:05:35 -0700

    Modify

--- SampleRepoSolution/SolutionEngine/SolutionEngine/ConfigurationEditor.Designer.cs
+++ SampleRepoSolution/SolutionEngine/SolutionEngine/ConfigurationEditor.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SolutionEngine
 {
-    partial class Form1
+    partial class ConfigurationEditor
     {
         /// <summary>
         /// Required designer variable.
--- SampleRepoSolution/SolutionEngine/SolutionEngine/ConfigurationEditor.cs
+++ SampleRepoSolution/SolutionEngine/SolutionEngine/ConfigurationEditor.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SolutionEngine
 {
-    public partial class Form1 : Form
+    public partial class ConfigurationEditor : Form
     {
-        public Form1()
+        public ConfigurationEditor()
         {
             InitializeComponent();
         }
